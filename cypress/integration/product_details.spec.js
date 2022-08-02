ec808c563371ae029ed08b37bfda3eb91a008f01
Shrey Patel <shreypatel11@hotmail.com>
2022-08-02 16:34:13 -0400

    Cypress Product Details spec

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,11 @@
+describe("Testing Home page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("should click on a product", () => {
+    cy.contains("Scented Blade").click()
+    cy.contains("The Scented Blade is an extremely rare, tall plant and can be found mostly in savannas. It blooms once a year, for 2 weeks.")
+  })
+  
+});
